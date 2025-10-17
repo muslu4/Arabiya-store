359af66ca37f1e6ea73b47d0451b039efe690c5a
mimistore3iq-boop <mimistore3iq@gmail.com>
2025-10-17 23:12:25 +0300

    use HashRouter to fix Instagram open issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // ✅ استخدم HashRouter
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
